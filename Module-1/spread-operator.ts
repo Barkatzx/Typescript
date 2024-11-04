a0da3b6dfe66d4c7fd9a58ee994cb8c240b38922
Barkatzx <barkatullah.zx@gmail.com>
2024-11-05 00:35:47 +0600

    Spread Operator

---
+++ Module-1/spread-operator.ts
@@ -0,0 +1,35 @@
+{
+  // Spread Operator (...)
+  //   The spread operator (...) is used to "spread out" elements of an array or properties of an object. This can be useful for:
+  // Copying arrays or objects
+  // Merging arrays or objects
+  // Passing elements of an array as individual arguments to a function
+  const array1 = [1, 2, 3];
+  const array2 = [4, 5, 6];
+
+  const copyArray = [...array1]; //copy array
+  const mergeArray = [...array1, ...array2]; //merge array
+
+  console.log(copyArray);
+  console.log(mergeArray);
+
+  // Spread Operator in Object
+  const person = { name: "Alice", age: 25 };
+  const job = {
+    title: "Developer",
+    company: "Tesla",
+  };
+
+  const copyPerson = { ...person };
+  const personWithJob = { ...person, ...job };
+
+  console.log(copyPerson);
+  console.log(personWithJob);
+
+  // Using Spread Operator to Pass the Arguments
+  const numbers = [10, 20, 30] as [number, number, number];
+  function add(a: number, b: number, c: number): number {
+    return a + b + c;
+  }
+  console.log(add(...numbers));
+}
