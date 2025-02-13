f5536b9da57e6d21bc5e976d3a04a53f3a1bd894
Barkat Ullah <barkatullah.zx@gmail.com>
2025-02-13 07:37:26 +0600

    constraints

--- 02. Explore Advance Type of TypeScript/06. constraints.ts
+++ 02. Explore Advance Type of TypeScript/06. constraints.ts
@@ -3,6 +3,23 @@
   In TypeScript, you can constrain generic types to ensure they meet specific requirements. This means the generic type must have certain properties or extend a specific type.
   */
 
+  // Without constraints:
+  function getLng<T>(arg: T): number {
+    return arg.length; // ❌ Error: Property 'length' does not exist on type 'T'
+  }
+
+  //Fix with a constraint to ensure T has a length property:
+
+  function getLength<T extends { length: number }>(arg: T): number {
+    return arg.length; // ✅ Works because T is constrained
+  }
+
+  // Usage:
+  getLength("hello"); // 5 (string has `length`)
+  getLength([1, 2, 3]); // 3 (array has `length`)
+  getLength({ length: 42 }); // 42
+  // getLength(123); ❌ Error: number has no `length`
+
   interface Student {
     id: number;
     name: string;
